d9e5b26b200b00519902cb3cd6cba2908a0e63b6
P0sseid0n <matheuspossidoniom@outlook.com>
2026-01-18 03:32:13 -0300

    feat export on deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,9 @@ set -e
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 export APP_DIR="$SCRIPT_DIR/"
 
+export BUN_INSTALL="$HOME/.bun"
+export PATH="$BUN_INSTALL/bin:$PATH"
+
 cd "$SCRIPT_DIR"
 git pull
 bun install
